ae2b56f40952d3553e71354a78785033c42665b7
Lucas Brandão <llucasbrandaodev@gmail.com>
2020-10-18 20:32:51 -0300

    Fix swagger basePackage

--- src/main/java/com/lucasbrandao/restaurantapi/configs/SwaggerConfig.java
+++ src/main/java/com/lucasbrandao/restaurantapi/configs/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 		.select()
-		.apis(RequestHandlerSelectors.basePackage("com.fitch.teste"))
+		.apis(RequestHandlerSelectors.basePackage("com.lucasbrandao.restaurantapi"))
 		.build()
 		.apiInfo(metaData())
 		.securitySchemes(Arrays.asList(apiKey()));
